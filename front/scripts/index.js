1fba313a4ef78987649202bb65252b25e6ff1820
cuellaradriana <adriana141293@gmail.com>
2024-03-12 08:52:12 -0300

    Homework 6. Axios with async/await

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,3 +1,16 @@
 const renderCards = require('./renderCards');
+const axios = require('axios');
 
-$.get('https://students-api.2.us-1.fl0.io/movies', renderCards);
+const fetchData = async () => {
+    try {
+        const data = await axios.get(
+            'https://students-api.up.railway.app/movies'
+        );
+        console.log(data.data);
+        renderCards(data.data);
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+fetchData();
